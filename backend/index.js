fa30dd1cb2097a9a34a26f747b8c68fd5d99df95
Gaganmangal <mangalgagan4@gmail.com>
2024-07-30 17:46:53 +0530

    Video-45

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,14 @@
 const connectToMongo = require("./db");
 connectToMongo();
-const express = require("express");//Import Express Module
-const app = express();//Create an Express Application
-const port = 3000;//Define a Port Number
+const express = require("express"); //Import Express Module
+const app = express(); //Create an Express Application
+const port = 3000; //Define a Port Number
 
-app.get("/", (req, res) => {
-  res.send("Hello Gaganfnr!");
-});//Define a Route
+app.use(express.json());
+
+app.use("/api/auth", require("./routes/auth")); //Define a Route
+app.use("/api/notes", require("./routes/notes")); //Define a Route
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
-});//Start the Server
+}); //Start the Server
