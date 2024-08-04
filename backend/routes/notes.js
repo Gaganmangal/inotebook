ed4d1e862b370c946eac21ff258b966ea1bbdba0
Gaganmangal <mangalgagan4@gmail.com>
2024-08-04 15:58:17 +0530

    Video-53

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -50,31 +50,36 @@ router.post(
 );
 //ROUTE 3:Update Notes using: PUT "/api/notes/Update/:id".Login required
 router.put("/Update/:id", fetchuser, async (req, res) => {
-  const { title, description, tag } = req.body;
-  const newNote = {};
-  if (title) {
-    newNote.title = title;
-  }
-  if (description) {
-    newNote.description = description;
-  }
-  if (tag) {
-    newNote.tag = tag;
-  }
+  try {
+    const { title, description, tag } = req.body;
+    const newNote = {};
+    if (title) {
+      newNote.title = title;
+    }
+    if (description) {
+      newNote.description = description;
+    }
+    if (tag) {
+      newNote.tag = tag;
+    }
 
-  let note = await Notes.findById(req.params.id);
-  if (!note) {
-    return res.status(404).send("Not Found");
+    let note = await Notes.findById(req.params.id);
+    if (!note) {
+      return res.status(404).send("Not Found");
+    }
+    if (note.user.toString() !== req.user.id) {
+      return res.status(401).send("Not Allowed");
+    }
+    note = await Notes.findByIdAndUpdate(
+      req.params.id,
+      { $set: newNote },
+      { new: true }
+    );
+    res.json({ note });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Some Error occurred");
   }
-  if (note.user.toString() !== req.user.id) {
-    return res.status(401).send("Not Allowed");
-  }
-  note = await Notes.findByIdAndUpdate(
-    req.params.id,
-    { $set: newNote },
-    { new: true }
-  );
-  res.json({ note });
 });
 
 module.exports = router;
