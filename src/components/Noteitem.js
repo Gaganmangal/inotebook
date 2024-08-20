7ff6ff18e295f99eb454c67dc9d5134b4703dd45
Gaganmangal <mangalgagan4@gmail.com>
2024-08-20 16:43:49 +0530

    Video-67

--- src/components/Noteitem.js
+++ src/components/Noteitem.js
@@ -1,19 +1,27 @@
 import React, { useContext } from "react";
 import NoteContext from "../context/notes/NoteContext";
 const Noteitem = (props) => {
-  const { deleteNote } = useContext(NoteContext);;
-  const { note } = props;
+  const { deleteNote } = useContext(NoteContext);
+  const { note, updateNote } = props;
   // create a Note Card
   return (
     <div className="col-md-3">
       <div className="card my-3">
         <div className="card-body">
           <h5 className="card-title">{note.title}</h5>
-          <p className="card-text">
-            {note.description}
-          </p>
-          <i className="fa-solid fa-trash mx-1"onClick={()=>{deleteNote(note._id)}}></i>
-          <i className="fa-solid fa-pen-to-square mx-2"></i>
+          <p className="card-text">{note.description}</p>
+          <i
+            className="fa-solid fa-trash mx-1"
+            onClick={() => {
+              deleteNote(note._id);
+            }}
+          ></i>
+          <i
+            className="far fa-edit mx-2"
+            onClick={() => {
+              updateNote(note);
+            }}
+          ></i>
         </div>
       </div>
     </div>
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -70,16 +70,29 @@ const NoteState = (props) => {
 
   // // Edit a Note
   const editNote = async (id, title, description, tag) => {
-    // API Call
+    // API CALL
+    const response = await fetch(`${host}/api/notes/Update/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        "auth-token":
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjZhZTAzNDU5ZGNkOWRlYjdjYzA5YTNhIn0sImlhdCI6MTcyMjc1NjcwNX0.CBgEYrgEdfJpfOp4JpBOTRyNJTLSogusK-nKKIsbdfs",
+      },
+    });
+    const json = await response.json();
+    console.log(json);
+    let newNotes = JSON.parse(JSON.stringify(notes));
     // Logic to edit in client
-    for (let index = 0; index < notes.length; index++) {
-      const element = notes[index];
+    for (let index = 0; index < newNotes.length; index++) {
+      const element = newNotes[index];
       if (element._id === id) {
-        element.title = title;
-        element.description = description;
-        element.tag = tag;
+        newNotes[index].title = title;
+        newNotes[index].description = description;
+        newNotes[index].tag = tag;
+        break;
       }
     }
+    setNotes(newNotes);
   };
 
   return (
